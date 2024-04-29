586c53755991078459d22301902d42f82fe968ec
jeonghun.seo <149282129+Leziiii@users.noreply.github.com>
2024-04-29 21:11:39 +0900

    4_29

--- 4_9/main2.js
+++ 4_9/main2.js
@@ -1,16 +1,16 @@
-// const boxEL = document.querySelector('.box');
-// boxEL.addEventListener('click',function() {
-//     console.log('clicked');
-//     boxEL.classList.add('active');
+const boxEL = document.querySelector('.box');
+boxEL.addEventListener('click',function() {
+    console.log('clicked');
+    boxEL.classList.add('active');
 
-//     let hasActive = boxEL.classList.contains('active');
-//     console.log(hasActive);
+    let hasActive = boxEL.classList.contains('active');
+    console.log(hasActive);
 
-//     boxEL.classList.remove('active');
-//     hasActive = boxEL.classList.contains('active');
-//     console.log(hasActive);
-// });
-// console.log(boxEL)
+    boxEL.classList.remove('active');
+    hasActive = boxEL.classList.contains('active');
+    console.log(hasActive);
+});
+console.log(boxEL)
 
 const boxELs = document.querySelectorAll('.box');
 //반복 가능한 객체(Array, Map, Set, String, TypedArray,Arguments) 
