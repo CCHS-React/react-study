33ce524b433a038b978bac938cc8cb1e8a6c48e7
CCHS-React <cchsreact@gmail.com>
2024-06-11 21:10:04 +0900

    6_11

---
+++ 6_11/firstapp/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+// import Say from './Say'
+import EventPratice from './EventPratice';
+
+const App = () => {
+  return <EventPratice />;
+};
+
+export default App;
