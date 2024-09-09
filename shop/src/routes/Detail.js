f3b67732559456d5fc1b6b70c4cb8d75578574e9
CCHS-React <cchsreact@gmail.com>
2024-09-09 20:08:40 +0900

    9_9

--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -1,5 +1,7 @@
+import {useState,useEffect } from 'react';
 import {useParams} from 'react-router-dom'
 import styled from 'styled-components'
+
 
 let Box = styled.div`
     padding : 20px;
@@ -11,10 +13,23 @@ let YelloBtn = styled.button`
     padding : 10px;
 `;
 function Detail (props) {
+    useEffect(() => {
+        console.log("안녕안녕");
+    });
+    let [count, setCount] = useState(0);
+
     let {id} = useParams();
     let myItem = props.shoes.find(function(x) {
         return x.id == id
     });
+
+    let [alert, setAlert] = useState(true)
+    useEffect(() => {
+        let timer = setTimeout(()=> {setAlert(false)}, 2000)
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [] )
 
     return (
         <div className="container">
@@ -30,8 +45,9 @@ function Detail (props) {
                 </div>
                 <div>
                     <Box>
-                        <YelloBtn bg="orange">오렌지버튼</YelloBtn>
+                        <YelloBtn bg="orange" onClick={()=> {setCount(count+1)}}>오렌지버튼</YelloBtn>
                         <YelloBtn bg="blue">파란 버튼</YelloBtn>
+                        {alert == true ? <YelloBtn>2초후 사라짐</YelloBtn> : null}
                     </Box>
                 </div>
             </div>
