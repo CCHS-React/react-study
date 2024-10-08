3a47eb07df735ebcfe8d1b469ae6a4fb6a93f079
cchs-react <jeonghun.seo@outlook.kr>
2024-10-08 20:10:02 +0900

    10_08

--- shop/src/store.js
+++ shop/src/store.js
@@ -2,15 +2,19 @@ import { configureStore,createSlice } from "@reduxjs/toolkit";
 
 let user = createSlice({
     name : 'user',
-    initialState : 'kim',
+    initialState : {name :'kim', age : 20},
     reducers : {
-        changeName(state) { //여기서 state는 기존 state의 값 
+        changeName(state,action) { //여기서 state는 기존 state의 값 
             // 여기는 state값을 변경하는 함수입니다.
-            return 'john' + state
+            state.name = action.payload
+        },
+        addAge(state, action) {
+            // state.age = state.age + 1
+            state.age += action.payload
         }
     }
 })
-export let {changeName} = user.actions
+export let {changeName,addAge} = user.actions
 // 위에 만든 이녀석은 사실 [user,setUser] = useState('kim');
 let stock = createSlice({
     name : 'stock',
@@ -19,10 +23,21 @@ let stock = createSlice({
 let cart = createSlice({
     name: 'cart',
     initialState : [
-        {id : 0, name : 'white and black', count : 2},
-        {id : 2, name : 'Grey Yordan', count : 1}
-    ]
+        {id : 0, name : 'white and black', count : 2,defaultCount :2},
+        {id : 2, name : 'Grey Yordan', count : 1,defaultCount :1}
+    ],
+    reducers : {
+        addCount(state, action) {
+            state[action.payload].count++
+        },
+        minusCount(state, action) {
+            if (state[action.payload].count > state[action.payload].defaultCount){
+                state[action.payload].count--
+            }
+        }
+    }
 })
+export let {addCount,minusCount} = cart.actions
 
 export default configureStore({
     // reducer 안에 state를 등록해주어야지 사용이 가능
