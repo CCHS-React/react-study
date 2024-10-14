48276d5ba07e0755b527bc6bfbb446717cd149b9
cchs-react <jeonghun.seo@outlook.kr>
2024-10-14 20:20:11 +0900

    10_14

--- shop/src/routes/Cart.js
+++ shop/src/routes/Cart.js
@@ -3,6 +3,24 @@ import { useDispatch, useSelector } from "react-redux";
 import { changeName,addAge, addCount, minusCount } from "../store";
 
 function Cart() {
+  //localstorage test
+  localStorage.setItem('데이터 이름','데이터'); //추가
+  localStorage.getItem('데이터이름') // 읽기
+  // localStorage.removeItem('데이터 이름') // 삭제;
+
+  //localstorage는 키, 밸류 문자만 저장하는 공간
+  // array나 object를 저장하고 싶다면??
+  // array/object ==> json 변환해서 저장하면 됩니다
+  // json ==> 따옴표 친 array 라고 생각하십쇼
+  
+  localStorage.setItem('obj',JSON.stringify({name:'kim'}))
+  // json은 문자 취급이기 때문에 그냥 박을수 잇다
+  // json로 저장한 데이터를 꺼내고 싶으면 
+  let aaa = localStorage.getItem('obj')
+  console.log(aaa)
+  let b = JSON.parse(aaa)
+  console.log(b)
+
   let state = useSelector((state) => state);
   let dispatch = useDispatch()
   return (
